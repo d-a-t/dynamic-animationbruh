7b7b7037c0868630d7adbb2733a4ab6c71b252fb
d-a-t <40614725+d-a-t@users.noreply.github.com>
2022-04-13 10:01:53 -0400

    bruh

--- Assets/Scripts/Character/FootCollab.cs
+++ Assets/Scripts/Character/FootCollab.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using DitzelGames.FastIK;
 public class FootCollab : MonoBehaviour
 {
+	public CharacterController Controller;
+	public StarterAssets.StarterAssetsInputs Input;
+
     [Header("Arms")]
     public FootPlanting RightArmPlant;
     public FootPlanting LeftArmPlant;
@@ -12,6 +16,33 @@ public class FootCollab : MonoBehaviour
     public FootPlanting RightLegPlant;
     public FootPlanting LeftLegPlant;
 
+    public void RaycastAll(Vector3 direction) {
+        RaycastLeftArm(direction);
+        RaycastLeftFoot(direction);
+
+        RaycastRightArm(direction);
+        RaycastRightFoot(direction);
+    }
+
+    public void RaycastLeftArm(Vector3 direction) {
+        LeftArmPlant.ConeDirection = direction.normalized;
+        LeftArmPlant.RayCastUpdate();
+    }
+
+    public void RaycastLeftFoot(Vector3 direction) {
+        LeftLegPlant.ConeDirection = direction.normalized;
+        LeftLegPlant.RayCastUpdate();
+    }
+
+    public void RaycastRightArm(Vector3 direction) {
+        RightArmPlant.ConeDirection = direction.normalized;
+        RightArmPlant.RayCastUpdate();
+    }
+
+    public void RaycastRightFoot(Vector3 direction) {
+        RightLegPlant.ConeDirection = direction.normalized;
+        RightLegPlant.RayCastUpdate();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -23,13 +54,26 @@ public class FootCollab : MonoBehaviour
         LeftLegPlant.ConeDirection = (new Vector3(0, -1, 1)).normalized;
     }
 
+    private Listener<float> StillFootPlantingUpdate;
 
-    public float step = 0;
     // Update is called once per frame
     void Update()
     {
-        step += .05F;
+        if (Input.move == Vector2.zero) {
+            RaycastAll(Vector3.down);
 
-
+            if (StillFootPlantingUpdate.Destroyed) {
+                StillFootPlantingUpdate = Runservice.RunEvery
+                (
+                    Global.RunservicePriority.RenderStep.Character, 1, 
+                    (float dt) => {
+                        RaycastAll(Vector3.down);
+                        return true;
+                    }
+                );
+            }
+        } else {
+            StillFootPlantingUpdate.Destroy();
+        }
     }
 }
